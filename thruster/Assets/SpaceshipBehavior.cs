176cbf04c631f67d1f7ff9d9c35f3c41e3b64e28
ChoiJ0527 <ChoiJ0527@g.ucla.edu>
2020-07-19 23:19:21 +0900

    Added Unity project files
    Added Unity project initialization files, as well as a small demo to play around with.

---
+++ thruster/Assets/SpaceshipBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipBehavior : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yAxis = Input.GetAxis("Vertical");
+        float xAxis = Input.GetAxis("Horizontal");
+
+        ThrustUp(yAxis);
+        ThrustSide(xAxis);
+    }
+
+    private void ThrustUp(float amount)
+    {
+        Vector2 force = (transform.up * amount * 30);
+        rb.AddForce(force);
+    }
+
+    private void ThrustSide(float amount)
+    {
+        Vector2 force = (transform.right * amount * 30);
+        rb.AddForce(force);
+    }
+
+
+}
